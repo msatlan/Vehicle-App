dc8d66e608689cc9c993d46675c32e3bad50c87d
Marko Satlan <markosatlan@gmail.com>
2018-11-14 09:00:27 +0100

    inital database setup, inital UI setup(template cleared)

---
+++ VehicleApp/Models/VehicleMake.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace VehicleApp.Models
+{
+    public class VehicleMake
+    {
+        public int ID { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9'\s-]*$"), Required, StringLength(20)]
+        public string Name { get; set; }
+
+        [StringLength(10)]
+        public string Abrv { get; set; }
+
+        // Navigation property
+        public ICollection<VehicleModel> VehicleModels { get; set; }
+    }
+}
---
+++ VehicleApp/Models/VehicleModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehicleApp.Models
+{
+    public class VehicleModel
+    {
+        public int ID { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9'\s-]*$"), Required, StringLength(20)]
+        public string Name { get; set; }
+
+        [StringLength(10)]
+        public string Abrv { get; set; }
+
+        // Foreign key
+        public int VehicleMakeID { get; set; }
+
+        // Navigation property
+        public VehicleMake VehicleMake { get; set; }
+    }
+}
+
